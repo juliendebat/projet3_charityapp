61b09bf22fd8df098b2863e249cef59d1322488a
juliendebat <juliendebat09@gmail.com>
2021-09-23 12:58:41 +0200

    dossiers config + page accueil css html

--- src/main/java/fr/isika/projet3/config/WebMvcConfig.java
+++ src/main/java/fr/isika/projet3/config/WebMvcConfig.java
@@ -1,5 +1,20 @@
 package fr.isika.projet3.config;
 
-public class WebMvcConfig {
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "fr.isika.projet3")
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp().prefix("/WEB-INF/views/").suffix(".jsp");
+    }
+    
 
 }
